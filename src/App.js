b040f8542b1447ac5a9f54a798aaae64851efd7d
likam <mincheva.iglika@gmail.com>
2024-01-30 10:41:31 +0200

    move dropdown menu to separate component

--- src/App.js
+++ src/App.js
@@ -1,61 +1,9 @@
-import './App.scss';
+
+import { DropdownMenu } from './components/dropdown-menu/DropdownMenu';
 
 function App() {
   return (
-    <nav>
-      <ul>
-      <li>menu1
-        <ul className='drop-menu menu-1'> 
-        <li>one</li>
-        <li>two</li>
-        <li>tree</li>
-        <li>four</li>
-        <li>five</li>
-        <li>six</li>
-        </ul>
-      </li>
-      <li>menu 2
-        <ul className='drop-menu menu-2'> 
-        <li>one</li>
-        <li>two</li>
-        <li>tree</li>
-        <li>four</li>
-        <li>five</li>
-        <li>six</li>
-        </ul>
-      </li>
-      <li>menu 3
-        <ul className='drop-menu menu-3'> 
-        <li>one</li>
-        <li>two</li>
-        <li>tree</li>
-        <li>four</li>
-        <li>five</li>
-        <li>six</li>
-        </ul>
-      </li>
-      <li>menu 4
-        <ul className='drop-menu menu-4'> 
-        <li>one</li>
-        <li>two</li>
-        <li>tree</li>
-        <li>four</li>
-        <li>five</li>
-        <li>six</li>
-        </ul>
-      </li>
-      <li>menu 5
-        <ul className='drop-menu menu-5'> 
-        <li>one</li>
-        <li>two</li>
-        <li>tree</li>
-        <li>four</li>
-        <li>five</li>
-        <li>six</li>
-        </ul>
-      </li>
-      </ul>
-    </nav>
+   <DropdownMenu />
   );
 }
 
