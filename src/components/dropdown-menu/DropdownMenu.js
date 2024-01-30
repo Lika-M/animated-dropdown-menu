8622c702def15d20a65292cdb01df946c31c6080
likam <mincheva.iglika@gmail.com>
2024-01-30 10:42:27 +0200

    move dropdown menu to separate component

---
+++ src/components/dropdown-menu/DropdownMenu.js
@@ -0,0 +1,61 @@
+import './DropdownMenu.scss';
+
+export const DropdownMenu = () => {
+
+  return (
+    <nav>
+      <ul>
+        <li>menu1
+          <ul className='drop-menu menu-1'>
+            <li>one</li>
+            <li>two</li>
+            <li>tree</li>
+            <li>four</li>
+            <li>five</li>
+            <li>six</li>
+          </ul>
+        </li>
+        <li>menu 2
+          <ul className='drop-menu menu-2'>
+            <li>one</li>
+            <li>two</li>
+            <li>tree</li>
+            <li>four</li>
+            <li>five</li>
+            <li>six</li>
+          </ul>
+        </li>
+        <li>menu 3
+          <ul className='drop-menu menu-3'>
+            <li>one</li>
+            <li>two</li>
+            <li>tree</li>
+            <li>four</li>
+            <li>five</li>
+            <li>six</li>
+          </ul>
+        </li>
+        <li>menu 4
+          <ul className='drop-menu menu-4'>
+            <li>one</li>
+            <li>two</li>
+            <li>tree</li>
+            <li>four</li>
+            <li>five</li>
+            <li>six</li>
+          </ul>
+        </li>
+        <li>menu 5
+          <ul className='drop-menu menu-5'>
+            <li>one</li>
+            <li>two</li>
+            <li>tree</li>
+            <li>four</li>
+            <li>five</li>
+            <li>six</li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  );
+}
