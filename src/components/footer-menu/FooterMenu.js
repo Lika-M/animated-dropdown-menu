00dfc6f4d71f47f117fe3eccf2d5e3669ba865e6
likam <mincheva.iglika@gmail.com>
2024-01-31 12:50:04 +0200

    refactor using useState hook to fix state-related issues

--- src/components/footer-menu/FooterMenu.js
+++ src/components/footer-menu/FooterMenu.js
@@ -8,36 +8,33 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import CommentIcon from '@mui/icons-material/Comment';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 
+import { useState } from 'react';
 import './FooterMenu.scss';
 
 export const FooterMenu = () => {
 
+    const [active, setActive] = useState('0')
+
     const handleClick = (e) => {
-        if (e.target.tagName === "LI" &&  !e.target.classList.contains('active')) {
-            Array.from(e.target.parentElement.children).forEach(ch => {
-                if(ch.classList.contains('active')){
-                    ch.classList.remove('active');
-                }
-            });
-            e.target.classList.add("active");
-        }
+       setActive(e.target.dataset.index)
+        console.log(active)
     }
 
     return (
         <>
             {/* Section 1 - Notification */}
             <section className="section-notification">
-                <ul className="nav" onClick={handleClick}>
-                    <li className="active"><HomeIcon /></li>
-                    <li><SearchIcon /></li>
-                    <li><StarBorderIcon />
+                <ul className="nav" onClick={ handleClick}>
+                    <li className={active === '0' ? "active" : ""} data-index={'0'}><HomeIcon /></li>
+                    <li className={active === '1' ? "active" : ""} data-index={'1'}><SearchIcon /></li>
+                    <li className={active === '2' ? "active" : ""} data-index={'2'}><StarBorderIcon />
                         <ul className="notifications">
                             <li><VisibilityIcon /><span>9</span></li>
                             <li><CommentIcon /><span>4</span></li>
                             <li><AccountCircleIcon /><span>6</span></li>
                         </ul>
                     </li>
-                    <li><FaceIcon /></li>
+                    <li className={active === '3' ? "active" : ""} data-index={'3'}><FaceIcon /></li>
                 </ul>
             </section>
             {/* Section 2 - Alert */}
